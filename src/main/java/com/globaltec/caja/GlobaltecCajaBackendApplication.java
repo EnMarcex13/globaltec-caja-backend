84c1662955c1648c0be8abff3d6d2f0ecaa5b60d
SistemasGT <esanez@globaltec.com.pe>
2022-09-26 11:31:02 -0500

    inicio del proyecto

---
+++ src/main/java/com/globaltec/caja/GlobaltecCajaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.globaltec.caja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobaltecCajaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobaltecCajaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globaltec/caja/GlobaltecCajaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globaltec.caja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobaltecCajaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
